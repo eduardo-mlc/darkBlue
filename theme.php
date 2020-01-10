1c6121a8698ca687c4875c78904da456217ca65d
eduardo-mlc <59738023+eduardo-mlc@users.noreply.github.com>
2020-01-10 12:35:02 -0500

    Update theme.php

--- theme.php
+++ theme.php
@@ -64,7 +64,21 @@
                 <?= $Wcms->footer() ?>
                 
     
-    		</div>
+    		</div><?php if (wCMS::$currentPage == 'contact'): ?>
+	<?php
+		global $contact_form_email;
+		$contact_form_email = "your.email@example.com";
+	?>
+
+	<div class="container marginTop20">
+		<div class="col-xs-12 col-md-6 col-md-offset-3">
+			<div id="contactform" class="grayFont" style="height: 265px;">
+         		  	<?php contact_form(); ?>
+			</div>
+		</div>
+	</div>
+<?php endif ?>
+            
     	</footer>
     
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
