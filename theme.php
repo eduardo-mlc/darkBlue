b0b09f6db11d4a0a3b53cee2de8fa416a0a333d2
eduardo-mlc <59738023+eduardo-mlc@users.noreply.github.com>
2020-01-10 12:03:08 -0500

    Update theme.php

--- theme.php
+++ theme.php
@@ -70,7 +70,7 @@
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <?= $Wcms->js() ?>
-        <?php echo contact_form(); ?>
+       
     
     </body>
 </html>
